bb8263023082f72212ca1240725ab423de2557b5
Motko <77466238+Motko222@users.noreply.github.com>
2025-11-18 13:08:38 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -18,7 +18,7 @@ bpd=$(journalctl -u $folder.service --since "1 day ago" --no-hostname -o cat | g
 bph=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat | grep -c -E "successfully validated")
 
 status="ok" && message="validated $bph blocks last hour" 
-[ $bph -eq 0 ] && systemctl restart $folder.service && status="warning" && message="restarted (no blocks last hour)";
+#[ $bph -eq 0 ] && systemctl restart $folder.service && status="warning" && message="restarted (no blocks last hour)";
 [ $errors -gt 500 ] && status="warning" && message="too many errors ($errors/h)";
 [ $service -ne 1 ] && status="error" && message="service not running";
 
